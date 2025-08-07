abfbc8797b46c0ae498beca7b1cd78be683f6ade
Diego Silva <diegosilva@jera.com.br>
2025-08-07 17:25:42 -0300

    [FIX] change duplicate link icon

--- lib/rails_admin_clone/action.rb
+++ lib/rails_admin_clone/action.rb
@@ -18,7 +18,7 @@ module RailsAdmin
         register_instance_option :controller do
           Proc.new do
             model_cloner  = RailsAdminClone::ModelCloner.new(@object)
-            custom_method = model_config.clone_config.custom_method
+            custom_method = model_config.try(:clone_config).try(:custom_method)
 
             if custom_method.present?
               @object = model_cloner.method_clone(custom_method)
@@ -46,7 +46,7 @@ module RailsAdmin
         end
 
         register_instance_option :link_icon do
-          'icon-copy fa fa-files-o'
+          'fa fa-copy'
         end
 
         register_instance_option :pjax? do
